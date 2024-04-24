498a8b084a3eff00a212fa20c9fbe0e1f7cd1cd2
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-04-24 23:25:22 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Mode from './components/Mode'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Skill from './components/Skill'
+import Profile from './components/Profile'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Mode/>
+      <Header/>
+      <Hero/>
+      <Skill/>
+      <Profile/>
+      <Projects/>
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
